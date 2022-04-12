7e07030337a2e29044c9917ac42a810c64704d2b
your-name <your-email-address>
2022-04-12 21:47:09 +0300

    js functions

---
+++ functions.js
@@ -0,0 +1,27 @@
+function addTwo(number) {
+    return number + 2;
+  }
+  
+  const finalAnswer = addTwo(5);
+  console.log(finalAnswer);
+
+
+
+  function greet(firstName, lastName, honorific, greeting) {
+    return `${greeting} ${honorific} ${lastName}! I’m extremely pleased you could join us, ${firstName}! I hope you enjoy your stay, ${honorific} ${lastName}.`;
+  }
+  
+  console.log(greet("Brian", "Holt", "Lord", "Salutations"));
+  console.log(greet("Jack", "Sparrow", "Captain", "A-hoy"));
+
+
+
+const myHomeCity = "Salt Lake City";
+const myHomeState = "Utah";
+const myHomeCountry = "USA";
+
+function logOutYourHome(city, state, country) {
+  console.log(`You are from ${city}, ${state} ${country}.`);
+}
+
+logOutYourHome(myHomeCity, myHomeState, myHomeCountry);
