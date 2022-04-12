ac4c6d08bc7aeed48c9eab5a25c32f3ad977c5b2
your-name <your-email-address>
2022-04-12 13:57:11 +0300

    if equals

--- ifStatements.js
+++ ifStatements.js
@@ -5,3 +5,11 @@ if (skyIsBlue) {
 } else {
   console.log("The sky is … not blue?");
 }
+
+const temperatureToday = 86;
+
+if (temperatureToday === "86") {
+  console.log("it is too damn hot");
+} else {
+  console.log("it is fine. maybe");
+}
